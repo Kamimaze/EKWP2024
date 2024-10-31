a6b62562c532350de5781a6d60dd4b3bbbdad547
unknown <IT-User@MI01-ITUM-03.CL1C.daa>
2024-10-31 13:27:20 +0100

    updated styles.css, modiefied <input> box length

--- login.php
+++ login.php
@@ -1,22 +1,20 @@
 <?php  require_once(".\layout\header.php") ?>
- <br>
- <br>
- <br>
- <br>
- <br>
- 
-    <div class="container">
-         <h1>Login</h1>
+    <div class="signup-section" id="signup">
+         <h1 class="container px-4 px-lg-5">Login</h1>
+         <br>
          <form>
-             <div class="form-group">
+             <div class="container px-4 px-lg-5">
                  <label for="username">Benutzername:</label>
                  <input type="text" class="form-control" id="username" placeholder="Enter username">
              </div>
-             <div class="form-group">
+             <br>
+             <div class="container px-4 px-lg-5">
                  <label for="password">Passwort:</label>
                  <input type="password" class="form-control" id="password" placeholder="Enter password">
+                 <br>
+                 <button class="btn btn-primary" id="submitButton " type="submit">Einloggen</button>
              </div>
-             <button type="submit" class="btn btn-primary">Einloggen</button>
+             
       </form>
     </div>
     <?php  require_once(".\layout\\footer.php") ?>
